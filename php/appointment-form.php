86f8ebeecc4005f83b508eeab8c280e9f562406d
Krystle Vollmer <krystlecmyk@gmail.com>
2015-01-31 15:28:46 -0600

    slider and forms working
    review carousel and forms are fixed. added some more styling and pics.

--- php/appointment-form.php
+++ php/appointment-form.php
@@ -2,29 +2,28 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Contact form</title>
+<title>Appointment Form</title>
 </head>
 
 <body>
-
 <?php
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $from = 'From: Website Contact Form'; 
+    $from = 'From: Website Appointment Form'; 
     $to = 'krystlecmyk@gmail.com, krystlecmyk@hotmail.com'; 
-    $subject = 'Website Contact Form';
+    $subject = 'Website Appointment Form';
     $human = $_POST['human'];
 			
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 				
-    if ($_POST['contact-submit'] && $human == '4') {				 
+    if ($_POST['appointment-submit'] && $human == '6') {				 
         if (mail ($to, $subject, $body, $from)) { 
 	    echo '<p>Your message has been sent!</p>';
 	} else { 
 	    echo '<p>Something went wrong, go back and try again!</p>'; 
 	} 
-    } else if ($_POST['contact-submit'] && $human != '4') {
+    } else if ($_POST['appointment-submit'] && $human != '6') {
 	echo '<p>You answered the anti-spam question incorrectly!</p>';
     }
 ?>
